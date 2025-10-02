6f0d97235d53d92c231bb0eaa3db9b8567d121c8
khaldoun36 <khaldoun36@gmail.com>
2025-10-02 09:53:17 +0400

    bug 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["@/assets/css/main.css"],
